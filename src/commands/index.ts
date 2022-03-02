829a55ad19a3ba04a39335107761121675e6916b
tchief <cyber.tchief@gmail.com>
2022-03-02 16:09:53 +0200

    Save matches to spreadsheet on /rewrite.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,6 +4,7 @@ import match from "./match";
 
 const composer = new Composer();
 
-composer.command("match", match);
+composer.command("match", (ctx) => match(ctx));
+composer.command("rewrite", (ctx) => match(ctx, true));
 
 export default composer;
--- src/commands/match/index.ts
+++ src/commands/match/index.ts
@@ -1,10 +1,11 @@
 import { Context } from "grammy";
 import { getMatches } from "./match";
+import { saveMatchesToRequestSheet } from "./sheets";
 import { Match, matchesToString } from "./types";
 
-const match = async (ctx: Context): Promise<void> => {
+const match = async (ctx: Context, rewrite = false): Promise<void> => {
     const matches: Match[] = await getMatches();
-    //await saveMatchesToRequestSheet(matches);
+    if (rewrite) await saveMatchesToRequestSheet(matches);
     await ctx.reply(matches.length ? `request: proposals\n\n${matchesToString(matches)}` : "No matches found yet.");
 };
 
